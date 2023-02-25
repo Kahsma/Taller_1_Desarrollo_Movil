9cccba71ed7b5e17f6436b4f900877b947f3d01b
Camilo Jose Martinez <cajomapa@gmail.com>
2023-02-24 19:39:29 -0500

    Pantalla de inicio + Guess game terminada

---
+++ app/src/main/java/com/javeriana/taller1/GuessGameActivity.kt
@@ -0,0 +1,11 @@
+package com.javeriana.taller1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuessGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_guess_game)
+    }
+}
