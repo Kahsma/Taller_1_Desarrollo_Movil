c89bfb9d12aad9329e40f407c1eb656a6b1b13c1
Camilo Jose Martinez <cajomapa@gmail.com>
2023-02-24 23:42:47 -0500

    Primera pantalla implementada

--- app/src/main/java/com/javeriana/taller1/GuessGameActivity.kt
+++ app/src/main/java/com/javeriana/taller1/GuessGameActivity.kt
@@ -2,10 +2,73 @@ package com.javeriana.taller1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.javeriana.taller1.databinding.ActivityGuessGameBinding
+import kotlin.random.Random
+
 
 class GuessGameActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGuessGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guess_game)
+        //setContentView(R.layout.activity_guess_game)
+
+
+        lBinding()
+
+
+
+
+    }
+
+    private fun lBinding(){
+
+        binding = ActivityGuessGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val intent = intent
+        val str: String? =intent.getStringExtra("Prompt")
+        var elContador = 0
+
+        binding.textViewRango.text = """Guess a Number between 0 and $str"""
+
+
+
+        val numeroAleatorio:Int =Random.nextInt(Integer.parseInt(str!!.trim()))
+        Log.v("Random", numeroAleatorio.toString())
+
+        binding.playButton.setOnClickListener {
+
+            val elNumero = Integer.parseInt(binding.editRespuesta.text.toString() )
+            if (elNumero < 0 || elNumero > Integer.parseInt(str.trim())){
+                binding.editRespuesta.error = "El numero no obedece al rango"
+                return@setOnClickListener
+            }
+
+            if(numeroAleatorio == elNumero ) {
+                binding.textViewHint.text = "Bien Hecho!"
+                binding.textViewRango.text = "=)"
+                binding.textViewCounter.text= ":D"
+
+            } else if (numeroAleatorio < elNumero ) {
+                elContador++
+                binding.textViewCounter.text = "Vas: $elContador Intentos!"
+                binding.textViewHint.text = "el numero $elNumero es mayor "
+            } else {
+                elContador++
+                binding.textViewCounter.text = "Vas: $elContador Intentos!"
+                binding.textViewHint.text = "el numero $elNumero es menor "
+
+            }
+
+        }
+
+
+
+    }
+
+    fun crearNumeroA (){
+
+
+
     }
 }
--- app/src/main/java/com/javeriana/taller1/MainActivity.kt
+++ app/src/main/java/com/javeriana/taller1/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.javeriana.taller1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import com.javeriana.taller1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        lBinding();
+
+    }
+
+    fun lBinding(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.ggB.setOnClickListener {
+
+            if(binding.editTextUpperLimitGG.text.toString().trim().isEmpty()||Integer.parseInt(binding.editTextUpperLimitGG.text.toString())<0||Integer.parseInt(binding.editTextUpperLimitGG.text.toString())>1000){
+                binding.editTextUpperLimitGG.error = "Numero invalido "
+
+                //Toast.makeText(baseContext,"Numero invalido",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener;}
+
+            startActivity(Intent(baseContext, GuessGameActivity::class.java).apply {
+                putExtra("Prompt", binding.editTextUpperLimitGG.text.toString())
+
+
+
+            })
+        }
+
+
+
+
+
+
     }
 }
